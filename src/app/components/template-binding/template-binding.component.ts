a9d4da9341dcdbcd8e02a572cfd942f930110d39
Mateus Sandes <mateussandes@hotmail.com.br>
2024-05-28 00:24:32 -0300

    Treinando os conceitas dentro dessa classe

--- src/app/components/template-binding/template-binding.component.ts
+++ src/app/components/template-binding/template-binding.component.ts
@@ -15,4 +15,18 @@ export class TemplateBindingComponent {
   public sum(val1: number, val2: number) {
     return val1 + val2;
   }
+
+  //essa propriedade vai ser usada para desabilitar um botão na
+  //pagina html
+  public isDisabled = false;
+
+  //fazendo o binding com uma imagem
+  //public srcValue = "https://www.istockphoto.com/br/foto/cidade-inteligente-com-linha-de-velocidade-brilhante-trilha-leve-ao-redor-da-cidade-gm1445614323-483963437?utm_source=pixabay&utm_medium=affiliate&utm_campaign=SRP_image_sponsored&utm_content=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Flink%2F&utm_term=link";
+
+  //usando construtores
+  // constructor() {
+  //   setTimeout(() => {
+  //     this.name = "Doutor Alex";
+  //   }, 1000)
+  // }
 }
