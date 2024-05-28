ba72bc986b72bf658adc0f262c32cf0abb5f0cd8
Mateus Sandes <mateussandes@hotmail.com.br>
2024-05-28 17:09:48 -0300

    Pratica de template variabbles

--- src/app/components/template-variables/template-variables.component.ts
+++ src/app/components/template-variables/template-variables.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { AfterViewInit, Component, ElementRef, ViewChild, viewChild } from '@angular/core';
+import { NewComponentComponent } from '../new-component/new-component.component';
 
 @Component({
   selector: 'app-template-variables',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    NewComponentComponent
+  ],
   templateUrl: './template-variables.component.html',
   styleUrl: './template-variables.component.css'
 })
@@ -11,9 +16,13 @@ export class TemplateVariablesComponent implements AfterViewInit{
 
   //pegando dados do template variable (ou seja, o #h2 que esta no html)
   @ViewChild('name') public nameInput!: ElementRef;
+  @ViewChild('h2') public h2!: ElementRef;
+  @ViewChild('NewComponent') public childComponent!: NewComponentComponent;
 
   //resgatando os valores da pagina
   ngAfterViewInit(): void {
     console.log(this.nameInput.nativeElement.value);
+    console.log(this.h2.nativeElement.value);
+    console.log(this.childComponent.name);
   }
 }
