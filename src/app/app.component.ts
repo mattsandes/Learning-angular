ad7b9b2b611e850998784938e95cb451d30359be
Mateus Sandes <mateussandes@hotmail.com.br>
2024-05-27 23:47:05 -0300

    Deletados as besteiras que eu fiz e modifiquei o app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NewComponentComponent } from './components/new-component/new-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    NewComponentComponent
+  ],
+  template:
+  `
+    <h1>Aprendendo angular</h1>
+    <app-new-component />
+  `,
 })
 export class AppComponent {
   title = 'learning-angular';
