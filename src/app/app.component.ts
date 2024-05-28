8312443d89567f51b83ca654e5da26ebf487445e
Mateus Sandes <mateussandes@hotmail.com.br>
2024-05-28 00:07:30 -0300

    Importadas as tags do componente template-binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NewComponentComponent } from './components/new-component/new-component.component';
+import { TemplateBindingComponent } from './components/template-binding/template-binding.component';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +10,16 @@ import { NewComponentComponent } from './components/new-component/new-component.
   imports: [
     RouterOutlet,
     CommonModule,
-    NewComponentComponent
+    NewComponentComponent,
+    TemplateBindingComponent
   ],
   template:
   `
     <h1>Aprendendo angular</h1>
     <app-new-component />
+
+    <app-template-binding />
+
   `,
 })
 export class AppComponent {
